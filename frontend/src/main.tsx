bf778acfd2721eef59679ee046e435b412c721b2
samihaAbdel <samkallel@hotmail.fr>
2023-10-03 16:47:29 +0100

    router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
 import './index.css'
+import HomePage from './pages/HomePage.tsx'
+import Productpage from './pages/Productpage.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<Productpage />} />
+      {/* <Route path="dashboard" element={<Dashboard />} />
+      ... etc. */}
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
